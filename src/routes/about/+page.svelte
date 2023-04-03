006e4b4436cae7fd91343741cfa33fcb1bf654ce
Vili Ikäheimo <vili.vikaheim@gmail.com>
2023-04-03 13:47:42 +0300

    Add about page

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,78 @@
+<div class="outer">
+    <div>
+        <h1>Vili Ikäheimo</h1>
+        <p class="infotext">Software Developer</p>
+    </div>
+    <p class="longtext">
+        Starting Software developer passionate about developing in Rust.
+        Knowledgeable in front-end, back-end, debugging and security.
+        Experience in many technologies, including React, Node, Express, MongoDB and gRPC.
+    </p>
+    <div class="socials">
+        <a href="https://www.linkedin.com/in/vili-ikaheimo/" class="fa fa-linkedin-square linkedin"> </a>
+        <a href="https://www.linkedin.com/in/vili-ikaheimo/" class="fa fa-twitter twitter"> </a>
+        <a href="https://www.linkedin.com/in/vili-ikaheimo/" class="fa fa-github github"> </a>
+        <a href="mailto:vili.vikaheim@gmail.com" class="fa fa-envelope email"> </a>
+    </div>
+    <img src={"images/empty.png"} alt="loading.." >
+</div>
+
+
+<style lang="scss">
+    .outer {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    h1 {
+        color: #00ADB5;
+        font-size: 4rem;
+        text-align: center;
+    }
+
+    .infotext {
+        font-size: 2rem;
+        text-align: center;
+    }
+
+    .longtext {
+        margin-top: 2rem;
+        font-size: 2rem;
+        text-align: center;
+        max-width: 1000px;
+    }
+
+    .socials {
+        margin-top: 1rem;
+        display: flex;
+        justify-content: space-around;
+        width: 360px;
+    }
+
+    img {
+        margin-top: 4rem;
+        max-height: 300px;
+        aspect-ratio: 1 / 1;
+        border-radius: 50%;
+        object-fit: cover;
+    }
+
+    a {
+        text-decoration: none;
+        font-size: 36px;
+        color: #00ADB5;
+    }
+
+    .linkedin {
+        color: #0077B5;
+    }
+
+    .twitter {
+        color: #1DA1F2;
+    }
+
+    .github {
+        color: #333
+    }
+</style>
