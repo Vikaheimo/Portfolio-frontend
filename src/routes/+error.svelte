cba439d71ec35de8f961fb1551b9a2b905c446de
Vili Ikäheimo <vili.vikaheim@gmail.com>
2024-08-25 11:42:43 +0300

    Add a custom 404 page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    import Button from '$lib/button.svelte';
+
+    const buttonOnClick = () => {
+        window.location.href = '/';
+    };
+</script>
+
+<div class="flex">
+    <div class="box">
+        <h1>The page you were searching for doesn't exist.</h1>
+        <Button text="Back to homepage" onClick={buttonOnClick} />
+    </div>
+</div>
+
+<style>
+    .flex {
+        display: flex;
+        align-self: center;
+        justify-content: center;
+    }
+    .box {
+        display: flex;
+        row-gap: 1rem;
+        flex-direction: column;
+        align-items: center;
+    }
+</style>
